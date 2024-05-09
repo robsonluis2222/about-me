2cf61f123359863a242dae757e25ede7eb6902c0
Robson <robsonluis2222@gmail.com>
2024-05-08 23:30:05 -0300

    first commit

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -6,7 +6,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const Home = () => {
   return (
     <div className='Home'>
-      <img className="profile-img" src="https://ideogram.ai/api/images/direct/4yBajasXStO8e8Ua0ZKkJA.png" alt="prof" />
+      <img className="profile-img" src="https://media.licdn.com/dms/image/D4E03AQGIM6SywZhUIw/profile-displayphoto-shrink_800_800/0/1712938486008?e=1720656000&v=beta&t=2Zo2pFSuAcE1o6TQF7FZ_ptgwII5COVBg7RiBVrKh70" alt="prof" />
       <div className='me'>
         <span className='my-name'>Robson L. Leite Jr.</span>
         <span className='am'>Web Front-End Developer</span>
@@ -32,15 +32,18 @@ const Home = () => {
       <div className='links'>
         <span className='titles-span'>My Links</span>
         <div class='btns-link'>
+          <a href="https://github.com/robsonluis2222/" target="_blank">
           <button className='github'>
               <span className='link-span'>git hub</span>
-          </button>
+          </button></a>
+          <a href="https://www.linkedin.com/in/robson-luis-95a039303?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">
           <button className='linkedin'>
               <span className='link-span'>Linkedin</span>
-          </button>
+          </button></a>
+          <a href="https://wa.me/5547996579387" target="_blank">
           <button className='whatsapp'>
               <span className='link-span'>whatsapp</span>
-          </button>
+          </button></a>
         </div>
       </div>
       <div className='techs-div'>
